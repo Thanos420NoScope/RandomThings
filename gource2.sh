458b1506a39578cdef65a3cfa3c7f89c6fb78e6f
Thanos420NoScope <rock.gauthier@outlook.fr>
2021-03-14 17:03:23 -0400

    Update gource2.sh

--- gource2.sh
+++ gource2.sh
@@ -9,7 +9,7 @@ OUT_MP4="video.mp4"
 gource -1280x720 --camera-mode track --hide progress --output-ppm-stream $OUT_PPM \
        --background-colour 000000 --font-colour 336699 --highlight-users --highlight-colour ff0000 \
        --auto-skip-seconds 1 --seconds-per-day 0.05 --title "hmm..." --user-scale 1 \
-       --user-image-dir tmp/avatars --bloom-multiplier 0.5 --bloom-intensity 0.5 --key
+       --user-image-dir tmp/avatars --bloom-multiplier 0.5 --bloom-intensity 0.5 --key \
        --file-extensions combo.log
 
 # Make video

5132fa2cedc31523225c9a71458cf6f97ba531ac
Thanos420NoScope <rock.gauthier@outlook.fr>
2021-03-15 01:11:35 -0400

    Update gource2.sh

--- gource2.sh
+++ gource2.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
 # Output temp
-OUT_PPM="/tmp/out.ppm"
+OUT_PPM="out.ppm"
 # Output
 OUT_MP4="video.mp4"
 
 # Create data
-gource -1280x720 --camera-mode overview --output-ppm-stream $OUT_PPM --font-colour 336699 \
+gource -1920x1080 -o $OUT_PPM --camera-mode overview --font-colour a290b1 \
        --background-image background.jpg --highlight-users --highlight-colour e0eeee \
-       --auto-skip-seconds 1 --seconds-per-day 0.2 --title "Kadena" --user-scale 1 \
-       --user-image-dir tmp/avatars --bloom-multiplier 0.5 --bloom-intensity 0.5 --key \
-       --file-extensions combo.log -e 0.5
+	   --auto-skip-seconds 0.01 --seconds-per-day 0.2 --title "Kadena" --user-scale 1.5 \
+	   --user-image-dir tmp/avatars --bloom-multiplier 0.2 --bloom-intensity 0.5 \
+	   --key --file-extensions combo.log --file-idle-time 0 --max-file-lag 1 \
+	   --hide filenames,progress,mouse --dir-name-depth 2 --multi-sampling --max-user-speed 1500 \
+	   --start-date "2016-09-30" --dir-name-position 1
 
 # Make video
-ffmpeg -y -r 25 -f image2pipe -vcodec ppm -i $OUT_PPM -vcodec libx264 -preset ultrafast \
+ffmpeg -y -r 60 -f image2pipe -vcodec ppm -i $OUT_PPM -vcodec libx264 -preset ultrafast \
        -pix_fmt yuv420p -crf 1 -threads 0 -bf 0 $OUT_MP4
 
 echo "check $OUT_MP4"

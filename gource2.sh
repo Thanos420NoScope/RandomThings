5c04402877a27e9997b75df189a7635e61fb4c5b
Thanos420NoScope <rock.gauthier@outlook.fr>
2021-03-15 20:53:33 -0400

    Update gource2.sh

--- gource2.sh
+++ gource2.sh
@@ -4,12 +4,17 @@
 OUT_PPM="out.ppm"
 # Output
 OUT_MP4="video.mp4"
+# Background
+BG="background.jpg"
+# Avatars
+AVA="tmp/avatars"
+
 
 # Create data
 gource -1920x1080 -o $OUT_PPM --camera-mode overview --font-colour a290b1 \
-       --background-image background.jpg --highlight-users --highlight-colour e0eeee \
+       --background-image $BG --highlight-users --highlight-colour e0eeee \
 	   --auto-skip-seconds 0.01 --seconds-per-day 0.2 --title "Kadena" --user-scale 1.5 \
-	   --user-image-dir tmp/avatars --bloom-multiplier 0.2 --bloom-intensity 0.5 \
+	   --user-image-dir $AVA --bloom-multiplier 0.2 --bloom-intensity 0.5 \
 	   --key --file-extensions combo.log --file-idle-time 0 --max-file-lag 1 \
 	   --hide filenames,progress,mouse --dir-name-depth 2 --multi-sampling --max-user-speed 1500 \
 	   --start-date "2016-09-30" --dir-name-position 1
